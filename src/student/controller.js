86116b1c818f1dc7a275214ba67a22b0214f485f
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-18 14:55:01 -0700

    adds student to database

--- src/student/controller.js
+++ src/student/controller.js
@@ -2,6 +2,26 @@
 const pool = require("../db");
 const queries = require("./queries");
 
+const addStudent = (req, res) => {
+  //pull data from request
+  const { name, email, age, dob } = req.body;
+  // check if email exists
+  pool.query(queries.checkEmailExists, [email], (error, results) => {
+    if (results.rows.length) {
+      res.send("Email already exists");
+    }
+    //add student to db if email doesnt exist
+    pool.query(
+      queries.addStudent,
+      [name, email, age, dob],
+      (error, results) => {
+        if (error) throw error;
+        res.status(201).send("student created successfully");
+      }
+    );
+  });
+};
+
 const getStudents = (req, res) => {
   pool.query(queries.getAllStudents, (error, results) => {
     if (error) {
@@ -18,17 +38,6 @@ const getStudentById = (req, res) => {
   pool.query(queries.studentById, [id], (error, results) => {
     if (error) throw error;
     res.status(200).json(results.rows);
-  });
-};
-
-const addStudent = (req, res) => {
-  //pull data from request
-  const { name, email, age, dob } = req.body;
-  // check if email exists
-  pool.query(queries.checkEmailExists, [email], (error, results) => {
-    if (results.rows.length) {
-      res.send("Email already exists");
-    }
   });
 };
 
--- src/student/queries.js
+++ src/student/queries.js
@@ -3,9 +3,12 @@
 const getAllStudents = "SELECT * FROM students";
 const studentById = "SELECT * FROM students WHERE id = $1";
 const checkEmailExists = "SELECT s FROM students s WHERE s.email = $1";
+const addStudent =
+  "INSERT INTO students (name,email,age,dob) VALUES ($1, $2, $3, $4)";
 
 module.exports = {
   getAllStudents,
   studentById,
   checkEmailExists,
+  addStudent,
 };
